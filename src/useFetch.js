57ff04e66d9cca2114e2b4cb08a72b042e11ac2c
sakuramarylou121300 <maryloucortez12@gmail.com>
2023-09-17 00:12:59 +0800

    lesson20(1)

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,25 +7,25 @@ const useFetch = (url) => {
 
     useEffect(()=>{
         setTimeout(()=> {
-         fetch(url)
-         .then(res =>{
-             if(!res.ok){
-                 throw Error('Could not fetch the data for that resource.')
-             }
-             return res.json();
-         })
-         .then(data => {
+            fetch(url)
+            .then(res =>{
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource.')
+                }
+                return res.json();
+            })
+            .then(data => {
             setData(data)
             setIsPending(false)
             setError(null)
-         }) 
-         .catch((err)=>{
+            }) 
+            .catch((err)=>{
             setIsPending(false)
             setError(err.message)
-         })
+            })
         }, 1000)
-     }, [url])
-     return{data, isPending, error}
+    }, [url])
+    return{data, isPending, error}
 }
 
 export default useFetch;
